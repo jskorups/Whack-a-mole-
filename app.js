11d4445df149ad5e192a68fe1d36b325493e8852
Jakub <skorupski.jakub@gmail.com>
2020-09-27 21:15:16 +0200

    Final commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const square = document.querySelectorAll(".square");
+const mole = document.querySelectorAll(".mole");
+const timeLeft = document.querySelector("#time-left");
+let score = document.querySelector("#score");
+
+let result = 0;
+let currentTime = timeLeft.textContent
+
+function randomSuare() {
+  square.forEach((className) => {
+    className.classList.remove("mole")
+  })
+  let randomPosition = square[Math.floor(Math.random() * 9)]
+  randomPosition.classList.add("mole");
+
+  hitPosition = randomPosition.id;
+}
+
+square.forEach(id =>{
+    id.addEventListener('mouseup', () => {
+        if(id.id === hitPosition) {
+            result += 1
+            score.textContent = result
+        }
+    })
+})
+
+
+function moveMole(){
+    let timerId = null
+    timerId = setInterval (randomSuare, 500)
+}
+
+moveMole()
+
+
+function countDown(){
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if(currentTime === 0){
+        clearInterval(timerId)
+        alert('Game over! Your score is ' +  result)
+    }
+}
+
+let timerId = setInterval(countDown, 1000)
+
